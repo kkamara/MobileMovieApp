e0599b21273db36c83414fee2fb3924c330eab66
kkamara <kamaracomputers@gmail.com>
2026-01-19 13:11:33 +0000

    Appwrite setup

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar'
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
 import useFetch from '@/services/useFetch'
 import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
@@ -24,6 +25,10 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies()
+
+        if (0 < movies?.length && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies[0])
+        }
       } else {
         reset()
       }
